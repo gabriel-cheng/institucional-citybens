056b4a73dd74c0bb5e221f73360cf97dc9ed0ba9
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2022-08-15 19:09:09 -0300

    Ending the day 15-08-22 :clap: :sparkles:

---
+++ public/script/scroll-effect.js
@@ -0,0 +1,7 @@
+const headerContainer = document.querySelector('#header-container');
+
+window.addEventListener('scroll', () => {
+    const scrolled = window.scrollY;
+
+    console.log('Scolado!');
+});
