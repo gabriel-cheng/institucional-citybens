7a55daa21a27c6f823fa670ae3c7f55b38bf8c2e
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2022-10-06 17:26:01 -0300

    :recycle: Refactor - Refatorando rotas da aplicação

--- server.js
+++ server.js
@@ -1,58 +1,27 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const router = require('./routes/index');
+
+(function configs() {
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());
+
+    app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+
+    app.use(express.static('public'));
+})();
+
+(function rotas() {
+    app.use('/', router);
+})();
+
 (function() {
-    const express = require('express');
-    const app = express();
-    const exphbs = require('express-handlebars');
-    const bodyParser = require('body-parser');
-
-    // Configs
-    (function() {
-        app.use(bodyParser.urlencoded({extended: false}));
-        app.use(bodyParser.json());
-
-        app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
-        app.set('view engine', 'handlebars');
-
-        app.use(express.static('public'));
-    })();
-
-    // Routes
-    (function() {
-        app.get('/filiais', (request, response) => {
-            response.render('filiais');
-        });
-
-        app.post('/disponibilidade', (request, response) => {
-            response.send(
-                '<h1>Recebi seus dados!</h1><br><br>' +
-                `Consórcio: ${request.body.consorcioDadosValue}<br>` +
-                `Valor: ${request.body.valorPagar}<br>` +
-                `Parcelas: ${request.body.valorParcelas}`
-            );
-        });
-
-        app.post('/simulacao', (request, response) => {
-            response.render('simulacao', {text: request.body.consorcioDadosValue});
-        });
-
-        app.post('/informe-seus-dados', (request, response) => {
-            response.render('informe-seus-dados', {text: request.body.sonho});
-        });
-
-        app.get('/consorcio', (request, response) => {
-            response.render('consorcio');
-        });
-
-        app.get('/', (request, response) => {
-            response.render('index');
-        });
-    })();
-
-    // Port config
-    (function() {
-        const port = process.env.PORT || 3000;
-        app.listen(port, () => {
-            console.log(`Server is running on port: ${port}`);
-            console.log(`Access: http://localhost:${port}`);
-        });
-    })();
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`);
+        console.log(`Access: http://localhost:${port}`);
+    });
 })();
