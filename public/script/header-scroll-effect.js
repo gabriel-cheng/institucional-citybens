952fa7b5726478b2a67169f751cc3511203e330d
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2022-12-20 20:35:44 -0300

    :sparkles: chore - adicionando ESlint ao projeto :fire:

--- public/script/header-scroll-effect.js
+++ public/script/header-scroll-effect.js
@@ -1,10 +1,10 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header-container").style.top = "0";
-  } else {
-    document.getElementById("header-container").style.top = "-90px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header-container").style.top = "0";
+    } else {
+        document.getElementById("header-container").style.top = "-90px";
+    }
+    prevScrollpos = currentScrollPos;
+};
--- public/script/phonemask.js
+++ public/script/phonemask.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /**
  * Library: neovav/phonemask
  *
@@ -8,13 +9,13 @@
  * Copyright © Verveda Oleksandr Viktorovich <neovav@outlook.com>
  */
 const phoneMask = function () {
-    const parent = this
+    const parent = this;
 
     let listDigits = [
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-    ]
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+    ];
 
-    let listTags = []
+    let listTags = [];
 
     /**
      * Check if a phone format handler is assigned to the specified tag
@@ -24,16 +25,16 @@ const phoneMask = function () {
      * @return {boolean}
      */
     function checkEvent (tag) {
-        let ret = false
-        const keys = Object.keys(listTags)
-        const len = keys.length
+        let ret = false;
+        const keys = Object.keys(listTags);
+        const len = keys.length;
         for (let i = 0; i < len; i += 1) {
-            const item = listTags[keys[i]]
+            const item = listTags[keys[i]];
             if (tag === item) {
                 ret = true;
             }
         }
-        return ret
+        return ret;
     }
 
     /**
@@ -50,17 +51,17 @@ const phoneMask = function () {
             if (document.selection) {
                 tag.focus ();
                 const Sel = document.selection.createRange();
-                Sel.moveStart('character', -ctrl.value.length);
+                Sel.moveStart("character", -ctrl.value.length);
                 CaretPos = Sel.text.length;
-            } else if ( tag.selectionStart || tag.selectionStart === '0' ) {
+            } else if ( tag.selectionStart || tag.selectionStart === "0" ) {
                 CaretPos = tag.selectionStart;
             }
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
 
         return CaretPos;
-    }
+    };
 
     /**
      * Setting the cursor position in the telephone input field
@@ -78,9 +79,9 @@ const phoneMask = function () {
                 tag.select();
             }
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
-    }
+    };
 
     /**
      * Search in a mask for a position to insert a digit
@@ -92,30 +93,30 @@ const phoneMask = function () {
      * @return {int}
      */
     this.searchPosition = function (tag, mask, step = 1) {
-        let ret = parent.getCursorPosition(tag)
+        let ret = parent.getCursorPosition(tag);
 
-        let arrMask = mask.split('')
-        let len = arrMask.length
+        let arrMask = mask.split("");
+        let len = arrMask.length;
 
-        let i = ret
-        let isFirst = true
+        let i = ret;
+        let isFirst = true;
 
         while (i >= 0 && i <= len) {
-            let char = arrMask[Math.min(i, i + step)]
-            if (char === '_') {
+            let char = arrMask[Math.min(i, i + step)];
+            if (char === "_") {
                 if (isFirst) {
-                    ret = i + step
+                    ret = i + step;
                 } else {
-                    ret = i
+                    ret = i;
                 }
-                break
+                break;
             }
-            i += step
-            isFirst = false
+            i += step;
+            isFirst = false;
         }
 
-        return ret
-    }
+        return ret;
+    };
 
     /**
      * Focus and click handler in the phone input field
@@ -125,15 +126,15 @@ const phoneMask = function () {
      */
     function correctPosition (tag, mask) {
         try {
-            let position = parent.getCursorPosition(tag)
-            if (mask.substring(position, position + 1) !== '_') {
-                let newPosition = parent.searchPosition(tag, mask, 1)
+            let position = parent.getCursorPosition(tag);
+            if (mask.substring(position, position + 1) !== "_") {
+                let newPosition = parent.searchPosition(tag, mask, 1);
                 if (position !== newPosition) {
-                    parent.setCursorPosition(tag, newPosition)
+                    parent.setCursorPosition(tag, newPosition);
                 }
             }
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
     }
 
@@ -147,40 +148,40 @@ const phoneMask = function () {
      * @return {void}
      */
     function paste (event, tag, mask) {
-        let value = tag.getAttribute('value')
-        let arrValue = value.split('');
-        let paste = (event.clipboardData || window.clipboardData).getData('text')
-        let pasteDigits = paste.replace(/\D/g,'')
-        if (pasteDigits === '') {
-            return
+        let value = tag.getAttribute("value");
+        let arrValue = value.split("");
+        let paste = (event.clipboardData || window.clipboardData).getData("text");
+        let pasteDigits = paste.replace(/\D/g,"");
+        if (pasteDigits === "") {
+            return;
         }
-        let pasteDigitsLen = paste.length
-        let lenMask = mask.length
-        let newPosition = parent.getCursorPosition(tag)
+        let pasteDigitsLen = paste.length;
+        let lenMask = mask.length;
+        let newPosition = parent.getCursorPosition(tag);
 
-        let arrMask = mask.split('')
-        let arrPaste = pasteDigits.split('')
+        let arrMask = mask.split("");
+        let arrPaste = pasteDigits.split("");
         let pos = 0;
 
         for (let i = newPosition; i < lenMask; i += 1) {
-            let char = arrMask[i]
-            let charPaste = arrPaste[pos]
-            if (char === '_' && charPaste !== '') {
-                arrValue[i] = arrPaste[pos]
-                pos += 1
+            let char = arrMask[i];
+            let charPaste = arrPaste[pos];
+            if (char === "_" && charPaste !== "") {
+                arrValue[i] = arrPaste[pos];
+                pos += 1;
                 if (pos >= pasteDigitsLen) {
-                    newPosition = i + 1
-                    break
+                    newPosition = i + 1;
+                    break;
                 }
             }
         }
 
-        value = arrValue.join('')
+        value = arrValue.join("");
 
-        tag.setAttribute('value', value)
-        tag.value = value
+        tag.setAttribute("value", value);
+        tag.value = value;
 
-        parent.setCursorPosition(tag, newPosition)
+        parent.setCursorPosition(tag, newPosition);
     }
 
     /**
@@ -193,56 +194,56 @@ const phoneMask = function () {
      * @return {int}
      */
     function keyDown (event, tag, mask) {
-        const lenMask = mask.length
-        let position = parent.getCursorPosition(tag)
+        const lenMask = mask.length;
+        let position = parent.getCursorPosition(tag);
         let newPosition = position;
         const keyName = event.key;
-        let value = tag.getAttribute('value')
+        let value = tag.getAttribute("value");
         if (listDigits.includes(keyName)) {
             if (position < lenMask) {
-                newPosition = parent.searchPosition(tag, mask, 1)
+                newPosition = parent.searchPosition(tag, mask, 1);
                 if (position !== newPosition) {
-                    const arrValue = value.split('');
+                    const arrValue = value.split("");
                     if (Math.abs(position - newPosition) > 1) {
-                        arrValue[newPosition] = keyName
-                        newPosition += 1
+                        arrValue[newPosition] = keyName;
+                        newPosition += 1;
                     } else {
-                        arrValue[position] = keyName
+                        arrValue[position] = keyName;
                     }
 
-                    value = arrValue.join('')
+                    value = arrValue.join("");
 
-                    tag.setAttribute('value', value)
-                    tag.value = value
+                    tag.setAttribute("value", value);
+                    tag.value = value;
                 }
             }
 
         } else if (event.keyCode === 8 || event.keyCode === 46) {
-            let step = 1
+            let step = 1;
             if (event.keyCode === 8) {
-                step = -1
+                step = -1;
             }
-            newPosition = parent.searchPosition(tag, mask, step)
+            newPosition = parent.searchPosition(tag, mask, step);
             if (Math.abs(position - newPosition) === 1) {
-                const arrValue = value.split('');
+                const arrValue = value.split("");
                 if (step === -1) {
-                    arrValue[newPosition] = '_'
+                    arrValue[newPosition] = "_";
                 } else {
-                    arrValue[position] = '_'
+                    arrValue[position] = "_";
                 }
 
-                value = arrValue.join('')
+                value = arrValue.join("");
 
-                tag.setAttribute('value', value)
-                tag.value = value
+                tag.setAttribute("value", value);
+                tag.value = value;
             }
         } else if (event.keyCode === 37) {
-            newPosition = parent.searchPosition(tag, mask, -1)
+            newPosition = parent.searchPosition(tag, mask, -1);
         } else if (event.keyCode === 39) {
-            newPosition = parent.searchPosition(tag, mask, 1)
+            newPosition = parent.searchPosition(tag, mask, 1);
         }
         if (position !== newPosition) {
-            parent.setCursorPosition(tag, newPosition)
+            parent.setCursorPosition(tag, newPosition);
         }
     }
 
@@ -254,45 +255,47 @@ const phoneMask = function () {
      * @return {int}
      */
     this.init = function (css) {
-        const list = document.querySelectorAll(css)
-        const keys = Object.keys(list)
-        const len = keys.length
+        const list = document.querySelectorAll(css);
+        const keys = Object.keys(list);
+        const len = keys.length;
         for (let i = 0; i < len; i += 1) {
-            const tag = list[keys[i]]
+            const tag = list[keys[i]];
             try {
                 if (!checkEvent(tag)) {
-                    const mask = tag.getAttribute('mask')
-                    tag.value = mask
+                    const mask = tag.getAttribute("mask");
+                    tag.value = mask;
 
-                    listTags.push(tag)
-                    tag.addEventListener('paste', (event) => {
-                        paste (event, tag, mask)
+                    listTags.push(tag);
+                    tag.addEventListener("paste", (event) => {
+                        paste (event, tag, mask);
                         event.preventDefault();
-                    })
-                    tag.addEventListener('focus', () => {
-                        correctPosition (tag, mask)
-                        return true
-                    })
-                    tag.addEventListener('click', () => {
-                        correctPosition (tag, mask)
-                        return true
-                    })
-                    tag.addEventListener('keydown', (event) => {
-                        if (event.key === 'v') {
-                            return true
+                    });
+                    tag.addEventListener("focus", () => {
+                        correctPosition (tag, mask);
+                        return true;
+                    });
+                    tag.addEventListener("click", () => {
+                        correctPosition (tag, mask);
+                        return true;
+                    });
+                    tag.addEventListener("keydown", (event) => {
+                        if (event.key === "v") {
+                            return true;
                         }
 
-                        keyDown (event, tag, mask)
+                        keyDown (event, tag, mask);
 
-                        event.preventDefault()
-                        return false
+                        event.preventDefault();
+                        return false;
                     });
                 }
             } catch (e) {
-                console.log(e)
+                console.log(e);
             }
         }
 
-        return parent
-    }
-}
+        return parent;
+    };
+};
+
+module.exports = phoneMask;
