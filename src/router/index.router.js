7aeb060b07c321fff506d924cfb32b69ddb97ee1
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-01-19 13:57:36 -0300

    chore: desfazendo alterações de rotas do blog

--- src/router/index.router.js
+++ src/router/index.router.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 const controller = require("../controllers/index.controller");
 
 router.post("/api", controller.api);
-router.get("/filiais", controller.filiais);
 router.post("/disponibilidade", controller.disponibilidade);
 router.post("/simulacao", controller.simulacao);
 router.post("/informe-seus-dados", controller.informeSeusDados);
+router.get("/filiais", controller.filiais);
 router.get("/consorcio", controller.consorcio);
 router.get("/", controller.index);
 
